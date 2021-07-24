1a21b379108d60100c791dadb7c901da09e0e913
vseguro <79714681+vseguro@users.noreply.github.com>
2021-07-24 00:46:17 -0500

    .

--- src/test/java/test/TestFutbolista.java
+++ src/test/java/test/TestFutbolista.java
@@ -10,10 +10,9 @@ import org.junit.jupiter.api.TestMethodOrder;
 import futbol.Futbolista;
 import futbol.Jugador;
 
-
 @TestMethodOrder(MethodOrderer.Alphanumeric.class)
 public class TestFutbolista {
-	
+
 	@Test
    	public void testConstructor(){
 		Futbolista f = new Futbolista("Alejo", 20, "Delantero") {
@@ -37,17 +36,17 @@ public class TestFutbolista {
 		}
 		assertTrue(ok, "Hay un problema con el constructor de la clase Futbolista");
     }
-	
+
 	@Test
-   	public void testConstructorVacio(){
+	public void testConstructorVacio() {
 		Futbolista f = new Futbolista() {
-			
+
 			@Override
 			public int compareTo(Object o) {
 				// TODO Auto-generated method stub
 				return 0;
 			}
-			
+
 			@Override
 			public boolean jugarConLasManos() {
 				// TODO Auto-generated method stub
@@ -55,47 +54,46 @@ public class TestFutbolista {
 			}
 		};
 		boolean ok = false;
-		if (f.getEdad() == 30 && f.getNombre().equals("Maradona") 
-				&& f.getPosicion().equals("delantero")) {
+		if (f.getEdad() == 30 && f.getNombre().equals("Maradona") && f.getPosicion().equals("delantero")) {
 			ok = true;
 		}
 		assertTrue(ok, "Hay un problema con el constructor de la clase Futbolista");
-    }
-	
+	}
+
 	@Test
-   	public void testToString(){
+	public void testToString() {
 		Futbolista f = new Futbolista() {
-			
+
 			@Override
 			public int compareTo(Object o) {
 				// TODO Auto-generated method stub
 				return 0;
 			}
-			
+
 			@Override
 			public boolean jugarConLasManos() {
 				// TODO Auto-generated method stub
 				return false;
 			}
 		};
-		String  comp = "El futbolista Maradona tiene 30, y juega de delantero";
+		String comp = "El futbolista Maradona tiene 30, y juega de delantero";
 		boolean ok = false;
 		if (f.toString().equals(comp)) {
 			ok = true;
 		}
 		assertTrue(ok, "Hay un problema en el metodo toString de la clase Futbolista");
-    }
-	
+	}
+
 	@Test
-   	public void testEquals(){
+	public void testEquals() {
 		Futbolista f2 = new Futbolista() {
-			
+
 			@Override
 			public int compareTo(Object o) {
 				// TODO Auto-generated method stub
 				return 0;
 			}
-			
+
 			@Override
 			public boolean jugarConLasManos() {
 				// TODO Auto-generated method stub
@@ -103,48 +101,48 @@ public class TestFutbolista {
 			}
 		};
 		Futbolista f1 = new Futbolista() {
-			
+
 			@Override
 			public int compareTo(Object o) {
 				// TODO Auto-generated method stub
 				return 0;
 			}
-			
+
 			@Override
 			public boolean jugarConLasManos() {
 				// TODO Auto-generated method stub
 				return false;
 			}
 		};
-		
+
 		Futbolista f3 = f1;
 		boolean ok = false;
 		if (!f1.equals(f2) && f1.equals(f3)) {
 			ok = true;
 		}
 		assertTrue(ok, "Hay un problema en el metodo equals de la clase Futbolista");
-    }
-	
+	}
+
 	@Test
-   	public void testJugarManos(){
+	public void testJugarManos() {
 		Futbolista f = new Futbolista() {
-			
+
 			@Override
 			public int compareTo(Object o) {
 				// TODO Auto-generated method stub
 				return 0;
 			}
-			
+
 			@Override
 			public boolean jugarConLasManos() {
 				return true;
 			}
 		};
-		
+
 		boolean ok = false;
 		if (f.jugarConLasManos()) {
 			ok = true;
 		}
 		assertTrue(ok, "Hay un problema en el metodo jugarConLasManos de la clase Futbolista");
-    }
+	}
 }
